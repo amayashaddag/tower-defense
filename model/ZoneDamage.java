8826b5eb01e86e1790616262ee5619ec1c1c5e55
HADDAG Amayas <amayas.haddag@etu.u-paris.fr>
2023-11-20 19:47:31 +0100

    Another edit

--- model/ZoneDamage.java
+++ model/ZoneDamage.java
@@ -4,11 +4,4 @@ import java.util.List;
 
 public interface ZoneDamage {
     void affectZone(List<Mob> targetMobs); // x et y représentent le centre de la zone à attaquer
-    /* Vaut mieux l'appeler affectZone car à noter que cette fonction peut être appelée
-     * par soit les slowing down towers soit les attacking towers (idem pour les items)
-     * donc vaut mieux l'appeler ainsi.
-     * 
-     * A noter également que affectZone fera sûrement appel aux méthodes attack ou slowDown de l'interface
-     * selon le type de l'objet
-     */
 }
